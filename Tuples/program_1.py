df8b20b567b2fce4eabe547eeb92028766ffbd57
4Shivaraj <4shivaraj.gowda@gmail.com>
2022-10-08 11:15:56 +0530

    [Shivaraj] Add: Tuples data structures programs

---
+++ Tuples/program_1.py
@@ -0,0 +1,37 @@
+'''
+    @Author: Shivaraj
+    @Date: 07-10-2022
+    @Last Modified by: Shivaraj
+    @Last Modified date: 07-10-2022
+    @Title: Write a Python program to create a tuple.
+'''
+
+from data_log import get_logger
+
+lg = get_logger(name="(program_1)", file_name="data_log.log")
+
+
+class Tuples:
+    """
+    Tuple Manipulations
+    """
+
+    def create_tuples(self, my_tuple):
+        """
+        Description:
+            Takes the parameter as my_tuple and return printing the tuple data.
+        Parameter:
+            Passed parameter my_tuple as tuple. 
+        Return:
+            Returns printing the tuple data.
+        """
+        try:
+            return lg.info(f"type of data:{type(my_tuple)} and data: {my_tuple}")
+        except Exception as e:
+            lg.exception(e)
+
+
+if __name__ == "__main__":
+    my_tuple = (1, 2, 3, 4, 5)
+    tuple_obj = Tuples()
+    result = tuple_obj.create_tuples(my_tuple)
